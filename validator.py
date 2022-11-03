407e0fb35e4eed6edb6f6010167bcc89e5d85fa2
Nishu.Bharti1 <Nishu.Bharti1@ibm.com>
2022-11-03 11:05:02 +0530

    add custom validator for settings

--- validator.py
+++ validator.py
@@ -2,9 +2,29 @@ from operator import truediv
 import yamale
 import uuid
 import re
-from yamale.validators import DefaultValidators
+from yamale.validators import DefaultValidators, Validator
+
+
+class Settings(Validator):
+    """ Custom Date validator """
+    tag = 'settings'
+
+    def __init__(self, *args, **kwargs):
+        super(Settings, self).__init__(*args, **kwargs)
+        self.validators = [val for val in args if isinstance(val, Validator)]
+
+    def _is_valid(self, value):
+        found=False
+        for i in value:
+            if i['name'] == "TF_VERSION":
+                found=True
+                return True
+            
+        if found ==  False:
+            raise ValueError('TF_VERSION must be passed in the parent settings as well as module settings')
 
 validators = DefaultValidators.copy()  # This is a dictionary
+validators[Settings.tag]=Settings
 schema = yamale.make_schema('./schema.yaml', validators=validators)
 
 #example data sets
